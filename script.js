39158c186afe2f177c1f8431e9842838eebbd3c4
Olya Ionga <olechka2888@gmail.com>
2023-02-21 19:03:24 +0200

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+let num1 = Number(prompt('Введіть перше число:'));
+let num2 = Number(prompt('Введіть друге число:'));
+let num3 = Number(prompt('Введіть третє число:'));
+
+let avg = (num1 + num2 + num3) / 3;
+
+alert(`Середнє арифметичне чисел ${num1}, ${num2} та ${num3} дорівнює ${avg}`);
